237fcdf275af5b88070b0779c1339546c835f4c7
jmsallan <jose.maria.sallan@gmail.com>
2018-04-27 14:25:01 +0200

    more work

--- TextAnalysis/TrumpTest.R
+++ TextAnalysis/TrumpTest.R
@@ -1,7 +1,10 @@
 # https://rpubs.com/pjmurphy/265713
 
-Needed <- c("tm", "SnowballCC", "RColorBrewer", "ggplot2", "wordcloud", "biclust", "cluster", "igraph", "fpc")
-install.packages(Needed, dependencies = TRUE)
+# https://stackoverflow.com/questions/47410866/r-inspect-document-term-matrix-results-in-error-repeated-indices-currently-not
+
+# https://stackoverflow.com/questions/45738060/when-using-termdocumentmatrix-no-applicable-method-for-meta-applied-to-an-o
+
+#install also SnowballC package
 
 library(tm)
 
@@ -13,84 +16,41 @@ summary(docs)
 inspect(docs[1])
 writeLines(as.character(docs[1]))
 
-#---pre-processing----
-
-#removing punctuation
-docs <- tm_map(docs, removePunctuation)
-
-#removing special characters from emails
-for (j in seq(docs)) {
-  docs[[j]] <- gsub("/", " ", docs[[j]])
-  docs[[j]] <- gsub("@", " ", docs[[j]])
-  docs[[j]] <- gsub("\\|", " ", docs[[j]])
-  docs[[j]] <- gsub("\u2028", " ", docs[[j]])}
-
-#removing numbers
-docs <- tm_map(docs, removeNumbers)
-
-#converting to lowercase
-docs <- tm_map(docs, tolower)   
-docs <- tm_map(docs, PlainTextDocument)
-DocsCopy <- docs
-
-#removing stopwords
-docs <- tm_map(docs, removeWords, stopwords("english")) 
-docs <- tm_map(docs, PlainTextDocument)
-
-#removing specific words
-docs <- tm_map(docs, removeWords, c("syllogism", "tautology"))   
+#----pre-processing-----
 
 #binding words
 for (j in seq(docs))
 {
-  docs[[j]] <- gsub("fake news", "fake_news", docs[[j]])
-  docs[[j]] <- gsub("inner city", "inner-city", docs[[j]])
-  docs[[j]] <- gsub("politically correct", "politically_correct", docs[[j]])
+  docs[[j]] <- tm_mapgsub("fake news", "fakenews", docs[[j]])
+  docs[[j]] <- gsub("inner city", "innercity", docs[[j]])
+  docs[[j]] <- gsub("politically correct", "politicallycorrect", docs[[j]])
 }
-docs <- tm_map(docs, PlainTextDocument)
 
-#removing common word endings (didnt work)
-docs_st <- tm_map(docs, stemDocument)
-docs_st <- tm_map(docs_st, PlainTextDocument)
-writeLines(as.character(docs_st[1]))
+docs <- lapply(docs, function(x) tm_map(x, content_transformer(function(y) gsub(, pattern = "fake news", replacement = "fakenews"))))
+doc <- tm_map(doc, content_transformer(function(x) 
+  gsub(x, pattern = "buy", replacement = "bought")))
 
-docs_stc <- tm_map(docs_st, stemCompletion, dictionary = DocsCopy, lazy=TRUE)
-docs_stc <- tm_map(docs_stc, PlainTextDocument)
-writeLines(as.character(docs_stc[1]))
+
+#converting to lowercase
+docs <- tm_map(docs, content_transformer(tolower))
+
+#removing numbers
+docs <- tm_map(docs, removeNumbers)
+
+#removing stopwords
+docs <- tm_map(docs, removeWords, stopwords("english")) 
+
+#removing punctuation
+docs <- tm_map(docs, removePunctuation)
 
 #supress innecessary white spaces
 docs <- tm_map(docs, stripWhitespace)
-docs <- tm_map(docs, PlainTextDocument)
+
+docs <- tm_map(docs, stemDocument, language = "english")
 
 #----stage the data----
 
 # Create document matrix
-dtm <- DocumentTermMatrix(docs)   
+dtm <- DocumentTermMatrix(docs)
 inspect(dtm) #returns an error
 inspect(dtm[1:5, 1:20]) #returns an error
-
-
-# solution proposed on https://stackoverflow.com/questions/47410866/r-inspect-document-term-matrix-results-in-error-repeated-indices-currently-not
-cleantext <- function(corpus){
-  clean_corpus <- tm_map(corpus, removeNumbers)
-  clean_corpus <- tm_map(clean_corpus, content_transformer(tolower)) #!! modified
-  #clean_corpus <- tm_map(clean_corpus, PlainTextDocument) ### !!!! PlainTextDocument function erases metadata from corpus = document id! So this needs to be erased
-  #clean_corpus <- tm_map(clean_corpus, replacePunctuation)
-  clean_corpus <- tm_map(clean_corpus, removePunctuation)
-  #clean_corpus <- tm_map(clean_corpus, removeWords, c(stopwords("english"), myWords, top_names))
-  clean_corpus <- tm_map(clean_corpus, removeWords, c(stopwords("english")))
-  clean_corpus <- tm_map(clean_corpus, stripWhitespace)
-  clean_corpus <- tm_map(clean_corpus, stemDocument, language = "english")
-  
-  clean_corpus
-}
-
-#creating document term matrix
-docs0 <- VCorpus(DirSource("textsTrump"))
-clean_docs0 <- cleantext(docs0)
-dtm <- DocumentTermMatrix(clean_docs0)
-inspect(dtm)
-
-#creating term document matrix
-tdm <- TermDocumentMatrix(docs0)
-inspect(tdm)
